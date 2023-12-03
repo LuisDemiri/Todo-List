e8faf07c9cbdcb975a1bbc25c84d8f92dee8c1d0
Luis <luisdemiri05@icloud.com>
2023-12-03 17:00:09 +0100

    finished todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
+import { Checkbox } from 'antd';
 
-function App() {
+const App = () => {
+  const[task,setTask] = useState('')
+  const [allTasks,setAllTasks] = useState(JSON.parse(localStorage.getItem('allTasks')) || []);
+
+  const addTask=()=>{
+    let allTasksCopy = [...allTasks]
+
+    allTasksCopy.push(task)
+    setAllTasks(allTasksCopy)
+  }
+
+  const onChange = (taskId) => {
+
+    let allTasksCopy = [...allTasks]
+
+     allTasksCopy = allTasks.map((item) =>
+      item.id === taskId ? { ...item, completed: !item.completed } : item
+    );
+
+    setAllTasks(allTasksCopy);
+  
+  };
+
+  const onDelete =(taskId)=>{
+    let allTasksCopy = [...allTasks]
+     let allTasksUpdated = allTasksCopy.filter((item)=>item.id != taskId)
+     setAllTasks(allTasksUpdated)
+  }
+
+  useEffect(() => {
+    localStorage.setItem("allTasks",JSON.stringify(allTasks));
+  }, [allTasks])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input value={task.name} onChange={(e)=>setTask({name: e.target.value, id: Math.random(),completed: false})} />
+      <button onClick={()=>addTask()} >Add</button>
+
+      {allTasks?.map((item)=>{
+        return <div>
+        <Checkbox checked={item.completed} onChange={()=>onChange(item.id)}><p className={item.completed ? 'checked' : ""}>{item.name}</p></Checkbox>
+        <button onClick={()=>onDelete(item.id)}>Delete</button>
+        </div>
+      })}
+
     </div>
   );
 }
